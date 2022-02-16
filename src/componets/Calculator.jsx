397e47a64d20b49f274aa8afe35f57b9a0313e30
Chien0111 <xuanchien0111@gmail.com>
2022-02-16 22:43:30 +0700

    update

--- src/componets/Calculator.jsx
+++ src/componets/Calculator.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "../App.css";
 
 class Calculator extends React.Component {
-  number = [7,8,9,"+",,4,5,6,"-",1,2,3,"*",0,'.','=','/']
+  number = [7, 8, 9, "+", , 4, 5, 6, "-", 1, 2, 3, "*", 0, ".", "=", "/"];
+
+  state = {
+    item: "",
+  };
+
+  insertNumber = (newItem) =>{
+    console.log(newItem.taget.value);
+    this.setState({
+      item: this.state.item + newItem
+    })
+  }
+
   render() {
     return (
       <div className="maytinh">
@@ -12,7 +24,13 @@ class Calculator extends React.Component {
         <input type="text" placeholder="Kết quả" />
         <br />
         <button className="ac">AC</button>
-        
+        <div className="list-number">
+          {this.number.map((num) => (
+            <button onClick={this.insertNumber} key={num}>
+              {num}
+            </button>
+          ))}
+        </div>
       </div>
     );
   }
